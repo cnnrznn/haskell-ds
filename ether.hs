a7cf0014da9906f199df520fb5ec6f202de251b5
Connor Zanin <cnnrznn@gmail.com>
2019-09-26 19:39:29 -0400

    some safety, doesn't work

--- ether.hs
+++ ether.hs
@@ -6,11 +6,16 @@ data Event = Event { ident :: Int,
 send :: [Event] -> Event -> [Event]
 send ls e = ls ++ [e]
 
+headornah [] = Nothing
+headornah (x:xs) = Just x
+
 -- Remove a message for process p from the ether
 -- Messages are FIFO for a process p
 -- TODO consider selecting a message for p at random
-receive :: Int -> [Event] -> (Event, [Event])
-receive p ls = let forp = filter (\x -> ident x == p) ls
-                   event = head forp
-                   newls = tail forp ++ filter (/= event) ls
-               in (event, newls)
+-- TODO make this shit safe
+receive :: Int -> [Event] -> (Maybe Event, [Event])
+receive p ls =
+    let forp = filter (\x -> ident x == p) ls
+        event = headornah forp
+        newls = filter (/= event) ls
+    in (event, newls)
